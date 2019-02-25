3fd414bba184415f0963046e517a3e69bb1544e2
Erwann <leroy.erwann@gmail.com>
2019-02-25 21:56:14 +1100

    fix lighthouse

--- src/main/frontend/src/app/login/login.component.ts
+++ src/main/frontend/src/app/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
-  entrer(destination) {
+  entrer() {
     this.router.navigate(["/gestionbassin"], { skipLocationChange: true });
   }
 }
